36ec82e29dea44fb66d0b411e61d5d9fb0c025ba
Ahmed Rangel <ahmedrangel@outlook.com>
2025-04-27 15:56:27 -0500

    chore: remove unused scoped style

--- src/entrypoints/popup-ext/App.vue
+++ src/entrypoints/popup-ext/App.vue
@@ -7,18 +7,3 @@ import PopupCard from "@/components/PopupCard.vue";
     <PopupCard />
   </Suspense>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #54bc4ae0);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
